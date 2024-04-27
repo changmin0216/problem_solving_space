ebfedda93d9f24693acbcadd85d543d2571610dd
changmin <dufkawl0611@naver.com>
2024-04-27 14:33:21 +0900

    프로그래머스-2024/04/17

--- 자바_코테/algorithm/src/programmers/Main.java
+++ 자바_코테/algorithm/src/programmers/Main.java
@@ -3,40 +3,31 @@ package programmers;
 import java.util.*;
 
 public class Main {
-    public static int solution(int n, int[] lost, int[] reserve) {
-        int[] student = new int[n+1];
-        for(int i=0;i<lost.length;i++){
-            student[lost[i]] = -1;
-        }
-        for(int i=0;i<reserve.length;i++){
-            student[reserve[i]]+=1;
-        }
+    static int answer;
+    static int[] number;
+    static int size;
+    public static int solution(int[] numbers, int target) {
+        answer = 0;
+        number = numbers;
+        size = numbers.length;
 
-        for(int i=1;i<n+1;i++){
-            if (student[i] == -1){
-                if (student[i-1]>0){
-                    student[i]+=1;
-                    student[i-1]-=1;
-                    continue;
-                }
-                if (i!=n) {
-                    if (student[i + 1] > 0) {
-                        student[i] += 1;
-                        student[i + 1] -= 1;
-                    }
-                }
-            }
-        }
-        int answer = 0;
-        for (int i=1;i<n+1;i++){
-            if (student[i]>=0){
-                answer++;
-            }
-        }
+        dfs(0, 0, target);
         return answer;
     }
 
+    public static void dfs(int depth, int sum, int target){
+        if (depth==size){
+            if (sum==target){
+                answer++;
+            }
+            return;
+        }
+
+        dfs(depth+1, sum+number[depth], target);
+        dfs(depth+1, sum-number[depth], target);
+    }
+
     public static void main(String[] args){
-        System.out.println(solution(3,new int[]{3}, new int[]{1}));
+        System.out.println(solution(new int[]{1,1,1,1,1}, 3));
     }
 }
