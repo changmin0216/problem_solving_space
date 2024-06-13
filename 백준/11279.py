f256fd8a45a890c488e663d1c6adbcfb4977235c
changmin <dufkawl0611@naver.com>
2024-06-13 11:59:42 +0900

    백준-2024/6/13

---
+++ 백준/11279.py
@@ -0,0 +1,18 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+
+q = []
+
+for _ in range(n):
+    com = int(input())
+    if com!=0:
+        heapq.heappush(q, -com)
+    else:
+        if q:
+            print(-(heapq.heappop(q)))
+        else:
+            print(0)
+
