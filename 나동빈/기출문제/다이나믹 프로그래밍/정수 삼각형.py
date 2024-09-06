fa2d4303a6f974e2a82433e7e7ff138068e8b09d
changmin <dufkawl0611@naver.com>
2024-09-06 15:38:28 +0900

    이코테-2024/9/6

---
+++ 나동빈/기출문제/다이나믹 프로그래밍/정수 삼각형.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = []
+
+for _ in range(n):
+    dp.append(list(map(int, input().split())))
+
+for i in range(1, n):
+    for j in range(len(dp[i])):
+        if j==0:
+            dp[i][j] = dp[i][j] + dp[i-1][j]
+        elif j==len(dp[i])-1:
+            dp[i][j] = dp[i][j] + dp[i-1][j-1]
+        else:
+            dp[i][j] = dp[i][j] + max(dp[i-1][j], dp[i-1][j-1])
+print(max(dp[n-1]))
