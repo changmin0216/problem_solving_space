a1878a92b47969fba5cad405b2d193fa293ea7f1
changmin <dufkawl0611@naver.com>
2024-09-24 15:21:55 +0900

    백준-2024/9/24

--- 백준/9375.py
+++ 백준/9375.py
@@ -7,6 +7,12 @@ for _ in range(t):
     n = int(input())
     c = {}
     for _ in range(n):
-        name, category = map(str, input().rstrip())
-        c[category] = name
-    print(c)
+        name, category = map(str, input().split())
+        if c.get(category):
+            c[category].append(name)
+        else:
+            c[category] = [name]
+    result = 1
+    for i in c:
+        result*=(len(c[i])+1)
+    print(result-1)
