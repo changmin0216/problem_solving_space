fd0af0d7066737576bc3bf5b525a62cadba68a4f
changmin <dufkawl0611@naver.com>
2024-10-10 14:21:58 +0900

    백준-2024/10/10

--- 백준/11054.py
+++ 백준/11054.py
@@ -9,6 +9,7 @@ reverse_arr = arr[::-1]
 increase = [1 for i in range(n)] # 가장 긴 증가하는 부분 수열
 decrease = [1 for i in range(n)] # 가장 긴 감소하는 부분 수열(reversed)
 
+
 for i in range(n):
     for j in range(i):
         if arr[i] > arr[j]:
