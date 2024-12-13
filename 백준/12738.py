6ffd26238d97f1bc31e14511e9e07ba269254641
changmin <dufkawl0611@naver.com>
2024-12-13 11:44:47 +0900

    백준-2024/12/13

---
+++ 백준/12738.py
@@ -0,0 +1,17 @@
+import sys
+import bisect
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+lis = []
+
+for x in arr:
+    if not lis or x > lis[-1]:
+        lis.append(x)
+    else:
+        index = bisect.bisect_left(lis, x)
+        lis[index] = x
+
+print(len(lis))
