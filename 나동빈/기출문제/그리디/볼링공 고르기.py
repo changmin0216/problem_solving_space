1f96bd1d0ff6f3ee76aea42ef420f0fe7a08e2fa
changmin <dufkawl0611@naver.com>
2025-02-16 09:34:27 +0900

    코테-2024/02/16

--- 나동빈/기출문제/그리디/볼링공 고르기.py
+++ 나동빈/기출문제/그리디/볼링공 고르기.py
@@ -1,13 +1,14 @@
 import sys
 input = sys.stdin.readline
 
-n, m  = map(int, input().split())
+n, m = map(int, input().split())
 
-bowling = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
-cnt = 0
+result = 0
 for i in range(n-1):
-    for j in range(i+1, n):
-        if bowling[i]!=bowling[j]:
-            cnt+=1
-print(cnt)
+    for j in range(i, n):
+        if arr[i]!=arr[j]:
+            result+=1
+
+print(result)
