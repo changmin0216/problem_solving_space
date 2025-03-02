997adedd8e12a911cfec65c9a90f5bf1dd4f1b3f
changmin <dufkawl0611@naver.com>
2025-03-02 16:03:57 +0900

    백준-2024/03/02

--- 백준/2458.py
+++ 백준/2458.py
@@ -1,27 +1,31 @@
 import sys
 input = sys.stdin.readline
-
+INF = int(1e9)
 
 n, m = map(int, input().split())
 
-graph = [[0 for _ in range(n+1)] for _ in range(n+1)]
+graph = [[INF] * (n+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if i==j:
+            graph[i][j] = 0
 
 for _ in range(m):
-    short, tall = map(int ,input().split())
-    graph[short][tall]=1
+    a, b = map(int, input().split())
+    graph[a][b] = 1
 
 for k in range(1, n+1):
-    for i in range(1, n+1):
-        for j in range(1, n+1):
-            if graph[i][j] == 1 or (graph[i][k] == 1 and graph[k][j] == 1):
-                graph[i][j] = 1  # 자신보다 작은 경우
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
 
-#출력
-answer = 0
+result = 0
 for i in range(1, n+1):
-    known_height = 0
+    count = 0
     for j in range(1, n+1):
-        known_height += graph[i][j] + graph[j][i] #자신보다 작은사람과 큰사람의 합
-    if known_height == n-1: #자신의 키 순서를 알 경우
-        answer += 1
-print(answer)
+        if graph[i][j]!=INF or graph[j][i]!=INF:
+            count+=1
+    if count == n:
+        result+=1
+print(result)
