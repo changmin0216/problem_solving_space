c7d3c5e7f02e2ee38cbc0f2eaa5c6d4c481d7dee
changmin <dufkawl0611@naver.com>
2025-03-02 16:19:23 +0900

    이코태-2024/03/02

--- 나동빈/기출문제/최단경로/화성 탐사.py
+++ 나동빈/기출문제/최단경로/화성 탐사.py
@@ -1,37 +1,41 @@
 import sys
-# from collections import deque
 import heapq
 input = sys.stdin.readline
 INF = int(1e9)
 
-t = int(input())
-
-dx = [0,0,-1,1]
 dy = [-1,1,0,0]
+dx = [0,0,-1,1]
+def find_short_path(graph, N):
+    distance = [[INF] * N for _ in range(N)]
+    distance[0][0] = graph[0][0]
 
-for _ in range(t):
-    n = int(input())
-    g = []
-    for _ in range(n):
-        g.append(list(map(int, input().split())))
-
-    q = [(g[0][0], 0, 0)]
-    distance = [[INF] * n for _ in range(n)]
+    q = []
+    heapq.heappush(q, (0,0,distance[0][0]))
 
     while q:
-        dist, y, x = heapq.heappop(q)
+        y, x, dist = heapq.heappop(q)
 
         if distance[y][x] < dist:
             continue
 
         for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
+            ny, nx = y + dy[i], x + dx[i]
 
-            if 0 <= ny < n and 0 <= nx < n:
-                cost = dist + g[ny][nx]
-                if cost < distance[ny][nx]:
+            if 0<=ny<N and 0<=nx<N:
+                cost = dist + graph[ny][nx]
+                if distance[ny][nx] > cost:
                     distance[ny][nx] = cost
-                    heapq.heappush(q, (cost, ny, nx))
-    print(distance[n-1][n-1])
+                    heapq.heappush(q, (ny, nx, cost))
+    return distance[N-1][N-1]
 
+
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+
+    graph = []
+    for _ in range(N):
+        graph.append(list(map(int, input().split())))
+
+    print(find_short_path(graph, N))
