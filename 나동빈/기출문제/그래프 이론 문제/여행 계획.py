b20cf777aa1b11b037f38fa3112e922aef6110e7
changmin <dufkawl0611@naver.com>
2025-03-03 18:27:27 +0900

    이코태-2024/03/03

--- 나동빈/기출문제/그래프 이론 문제/여행 계획.py
+++ 나동빈/기출문제/그래프 이론 문제/여행 계획.py
@@ -1,17 +1,8 @@
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-
-graph = []
-parent = [0] * n
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
-
-plan = list(map(int, input().split()))
-
 def find_parent(parent, x):
-    if parent[x] != x:
+    if parent[x]!=x:
         parent[x] = find_parent(parent, parent[x])
     return parent[x]
 
@@ -19,25 +10,32 @@ def union_parent(parent, a, b):
     a = find_parent(parent, a)
     b = find_parent(parent, b)
 
-    if a < b:
+    if a<b:
         parent[b] = a
     else:
         parent[a] = b
 
-for i in range(n):
+n, m = map(int, input().split())
+
+parent = [0] * (n+1)
+
+for i in range(1, n+1):
     parent[i] = i
 
 for i in range(n):
+    com = list(map(int, input().split()))
+
     for j in range(n):
-        if graph[i][j] == 1:
-            union_parent(parent, i, j)
+        if com[j] == 1:
+            if find_parent(parent, i+1)!= find_parent(parent, j+1):
+                union_parent(parent, i+1, j+1)
 
-result = True
-for i in range(m-1):
-    if find_parent(parent, plan[i]-1) != find_parent(parent, plan[i+1]-1):
-        result = False
+journey = list(map(int, input().split()))
 
-if result:
-    print('YES')
+result = parent[journey[0]]
+for i in range(1, m):
+    if parent[journey[i]]!=result:
+        print("NO")
+        break
 else:
-    print('False')
+    print("YES")
