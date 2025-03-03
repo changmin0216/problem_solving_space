278f0db99ac77f02be072e582e60be9d6a3d621e
changmin <dufkawl0611@naver.com>
2025-03-03 17:25:32 +0900

    이코태-2024/03/03

--- 나동빈/개념/그래프 이론/커리큘럼.py
+++ 나동빈/개념/그래프 이론/커리큘럼.py
@@ -1,41 +1,34 @@
-import copy
 import sys
 from collections import deque
 input = sys.stdin.readline
 
-n = int(input())
+N = int(input())
 
-indegree = [0 for _ in range(n+1)]
+time = []
 
-graph = [[] for i in range(n+1)]
+graph = [[] for _ in range(N+1)]
 
-time = [0] * (n+1)
+indegree = [0] * (N+1)
 
-for i in range(1, n+1):
-    data = list(map(int, input().split()))
-    time[i] = data[0]
-    for x in data[1:-1]:
-        indegree[i]+=1
-        graph[x].append(i)
+for i in range(N):
+    a = list(map(int, input().split()))
 
-def topology_sort():
-    result = copy.deepcopy(time)
-    q = deque()
+    time.append(a[0]) ##각 강의의 시간
 
-    for i in range(1, n+1):
+    for j in range(1, len(a)-1):
+        graph[a[j]].append(i+1)
+        indegree[i+1]+=1
+
+q = deque()
+
+for i in range(1, len(indegree)):
+    if indegree[i] == 0:
+        q.append((i, time[i-1]))
+
+while q:
+    now, t, = q.popleft()
+    print(t)
+    for i in graph[now]:
+        indegree[i]-=1
         if indegree[i]==0:
-            q.append(i)
-
-    while q:
-        now = q.popleft()
-        for i in graph[now]:
-            result[i] = max(result[i], result[now] + time[i])
-            indegree[i]-=1
-
-            if indegree[i] == 0:
-                q.append(i)
-
-    for i in range(1, n+1):
-        print(result[i])
-
-topology_sort()
+            q.append((i, time[i-1]+t))
