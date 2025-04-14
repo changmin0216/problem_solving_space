5ac914a53611adbd62a473072662f64d71c669c2
changmin <dufkawl0611@naver.com>
2025-04-14 23:21:02 +0900

    삼성-2024/04/14

--- 코드트리/test.py
+++ 코드트리/test.py
@@ -1,336 +1,166 @@
-MAXN = 20
-MAXM = 20
-MAXF = 400
-MAXP = MAXF*6
+from collections import deque
+def find_route(si,sj,ei,ej):
+    q = deque()
+    v = [[0]*N for _ in range(N)]
 
-INF = int(1e9+10)
+    q.append((si,sj))
+    v[si][sj]=((si,sj))          # 직전위치를 저장
 
-SpaceMap = [[0 for _ in range(MAXN+10)] for _ in range(MAXN+10)]  # 미지의 공간의 평면도
-SpaceMapCellId = [[0 for _ in range(MAXN+10)] for _ in range(MAXN+10)]  # 평면도의 각 셀에 대응하는 그래프 정점의 번호를 저장하는 배열
-TimeWall = [[[0 for _ in range(MAXM+10)] for _ in range(MAXM+10)] for _ in range(6)]  # 시간의 벽의 단면도
-TimeWallCellId = [[[0 for _ in range(MAXM+10)] for _ in range(MAXM+10)] for _ in range(6)]  # 시간의 벽의 단면도의 각 셀에 대응하는 그래프 정점의 번호를 저장하는 배열
+    while q:
+        ci,cj = q.popleft()
 
-# 시간 이상 현상에 대한 정보를 저장할 클래스
-class AbnormalTimeEvent:
-    # 시간 이상 현상이 시작점의 행번호, 열번호, 방향, 확장 주기, 시간 이상 현상의 진행여부를 차례로 저장합니다
-    def __init__(self, xpos=0, ypos=0, direction=0, cycle=0, alive=0):
-        self.xpos = xpos
-        self.ypos = ypos
-        self.direction = direction
-        self.cycle = cycle
-        self.alive = alive
+        if (ci,cj)==(ei,ej):        # 목적지 도착! 경로 저장
+            route = []
+            ci,cj = v[ci][cj]
+            while (ci,cj)!=(si,sj): # 출발지가 아니라면 저장
+                route.append((ci,cj))
+                ci,cj = v[ci][cj]
+            return route[::-1]      # 역순(메두사 이동순서대로) 리턴
 
-events = [AbnormalTimeEvent() for _ in range(MAXF+10)]
+        # 네방향(상하좌우!), 범위내, 미방문, 조건(==0)
+        for di,dj in ((-1,0),(1,0),(0,-1),(0,1)):
+            ni,nj = ci+di, cj+dj
+            if 0<=ni<N and 0<=nj<N and v[ni][nj]==0 and arr[ni][nj]==0:
+                q.append((ni,nj))
+                v[ni][nj]=(ci,cj)
 
-# 그래프를 저장할 인접리스트
-Graph = []
+    # 이곳까지 왔다는 얘기는?? 목적지 못찾음
+    return -1
 
-# 그래프 생성 함수
-def build_graph(N, M):
-    global Graph, SpaceMapCellId, TimeWallCellId
+#     상,우상, 우,우하, 하,좌하, 좌,좌상
+#      0,  1,  2,  3,  4,  5,  6,  7
+di = [-1, -1, 0, 1, 1, 1, 0, -1]
+dj = [0, 1, 1, 1, 0, -1, -1, -1]
+
+def mark_line(v, ci, cj, dr):
+    while 0 <= ci < N and 0 <= cj < N:
+        v[ci][cj]=2                     # 시각적 구분위해 2로표시
+        ci,cj = ci+di[dr],cj+dj[dr]     # 해당 방향으로 한 칸 이동
+
+def mark_safe(v, si, sj, dr, org_dr):
+    # [1] 직선방향 표시
+    ci,cj = si+di[dr], sj+dj[dr]
+    mark_line(v, ci, cj, dr)        # v에 dr방향으로 이동가능지역 표시
+
+    # [2] 바라보는 방향으로 한줄씩 표시
+    ci,cj = si+di[org_dr],sj+dj[org_dr]
+    while 0<=ci<N and 0<=cj<N:          # 범위내라면 계속 진행
+        mark_line(v, ci, cj, dr)        # v에 dr방향으로 이동가능지역 표시
+        ci,cj = ci+di[org_dr],cj+dj[org_dr]
+
+#             tv, tstone = make_stone(marr,mi,mj,dr)
+def make_stone(marr, mi, mj, dr):
+    v = [[0]*N for _ in range(N)]
     cnt = 0
 
-    # 각 셀에 대해 대응될 번호를 차례로 부여합니다
-    # 평면도 중 시간의 벽이 아닌 부분의 셀들을 순회한 후,
-    # 단면도에 속하는 셀들을 단면도의 동쪽 -> 남쪽 -> 서쪽 -> 북쪽 -> 위쪽 순으로 셀들을 순회하면서 번호를 부여합니다
-    for i in range(N):
-        for j in range(N):
-            if SpaceMap[i][j] != 3:  # 시간의 벽이 있는 셀이 아닌 경우에만 번호를 부여합니다
-                cnt += 1
-                SpaceMapCellId[i][j] = cnt
+    # [1] dr 방향으로 >0 만날때까지 1표시, 이후좌표 2표시
+    ni,nj = mi+di[dr],mj+dj[dr]
+    while 0<=ni<N and 0<=nj<N:          # 범위내라면 계속 진행
+        v[ni][nj]=1
+        if marr[ni][nj] > 0:
+            cnt+=marr[ni][nj]
+            ni, nj = ni + di[dr], nj + dj[dr]
+            mark_line(v, ni, nj, dr)    # v에 dr방향으로 이동가능지역 표시
+            break
+        ni,nj = ni+di[dr], nj+dj[dr]
 
-    # 단면도의 동쪽, 남쪽, 서쪽, 북쪽, 위쪽 순으로 순회하며 셀에 번호를 부여합니다
-    for t in range(5):
-        for i in range(M):
-            for j in range(M):
-                cnt += 1
-                TimeWallCellId[t][i][j] = cnt
-
-    # 부여한 번호의 정점들로 구성된 그래프
-    # 정점의 번호에 대응되는 셀과 인접한 셀의 번호를 가지는 정점과 간선으로 연결합니다
-    # 최대 4개의 정점과 연결될 수 있습니다
-    # 평면도(단면도)에서, 오른쪽으로 인접한 경우 0, 아래쪽으로 인접한 경우 1, 왼쪽으로 인접한 경우 2, 위쪽으로 인접한 경우 3의 인덱스에 저장합니다
-    Graph = [[-1 for _ in range(4)] for _ in range(cnt+1)]
-
-    # 간선을 추가하는 작업을 위해 사용할 dx, dy 배열
-    # 동, 남, 서, 북에 대응되는 순서로 채워져 있습니다
-    dx = [0, 1, 0, -1]  # 동, 남, 서, 북
-    dy = [1, 0, -1, 0]
-
-    # 평면도에 속하는 셀에 대응되는 번호의 정점 쌍에 대해 간선을 추가합니다
-    for i in range(N):
-        for j in range(N):
-            if SpaceMap[i][j] != 3:  # 현재 셀에 장애물이 없는 경우
-                idx = SpaceMapCellId[i][j]
-                # 동, 남, 서, 북 순으로 인접한 셀들을 탐색합니다
-                for dd in range(4):
-                    nx = i + dx[dd]
-                    ny = j + dy[dd]
-                    # 범위를 벗어나면 넘어갑니다
-                    if nx < 0 or ny < 0 or nx >= N or ny >= N:
-                        continue
-                    # 셀에 장애물이 있는 경우 넘어갑니다
-                    if SpaceMap[nx][ny] == 3:
-                        continue
-                    # 그렇지 않은 경우, 이어줍니다
-                    Graph[idx][dd] = SpaceMapCellId[nx][ny]
-
-    # 단면도의 동쪽, 남쪽, 서쪽, 북쪽에 있는 셀들이 인접할 경우
-    # 대응되는 번호의 정점들을 이어줍니다
-    for t in range(4):
-        for i in range(M):
-            for j in range(M):
-                idx = TimeWallCellId[t][i][j]
-                # 위와 비슷하게 4방향 탐색
-                for dd in range(4):
-                    nx = i + dx[dd]
-                    ny = j + dy[dd]
-                    # 행 범위가 넘어갔을 경우 통과
-                    if nx < 0 or nx >= M:
-                        continue
-                    # 열 번호가 0보다 작아질 경우, 시계방향순으로 하나 앞에 있는 단면도의 가장 오른쪽 열의 셀과 인접합니다
-                    if ny < 0:
-                        Graph[idx][dd] = TimeWallCellId[(t+1)%4][nx][M-1]
-                    elif ny >= M:
-                        Graph[idx][dd] = TimeWallCellId[(t+3)%4][nx][0]
-                    else:
-                        Graph[idx][dd] = TimeWallCellId[t][nx][ny]
-
-    # 위쪽 단면도에 속하는 셀들에 대응되는 번호의 정점 쌍에 대해 간선을 추가합니다
-    for i in range(M):
-        for j in range(M):
-            idx = TimeWallCellId[4][i][j]
-            for dd in range(4):
-                nx = i + dx[dd]
-                ny = j + dy[dd]
-                # 범위를 벗어날 경우 넘어갑니다
-                if nx < 0 or ny < 0 or nx >= M or ny >= M:
-                    continue
-                # 그렇지 않을 경우 이어줍니다
-                Graph[idx][dd] = TimeWallCellId[4][nx][ny]
-
-    # 위쪽 단면도와 인접한 동쪽, 남쪽, 서쪽, 북쪽 단면도의 셀들에 대해
-    # 대응되는 번호의 정점을 잇는 간선을 추가합니다
-    # 동쪽 단면도와 인접한 셀들의 경우
-    for i in range(M):
-        idx = TimeWallCellId[4][i][M-1]  # 인접한 위쪽 단면도의 셀의 번호
-        idy = TimeWallCellId[0][0][M-1 - i]  # 인접한 동쪽 단면도의 셀의 번호
-        Graph[idx][0] = idy
-        Graph[idy][3] = idx
-    # 남쪽 단면도와 인접한 셀들의 경우
-    for i in range(M):
-        idx = TimeWallCellId[4][M-1][i]  # 인접한 위쪽 단면도의 셀의 번호
-        idy = TimeWallCellId[1][0][i]  # 인접한 남쪽 단면도의 셀의 번호
-        Graph[idx][1] = idy
-        Graph[idy][3] = idx
-    # 서쪽 단면도와 인접한 셀들의 경우
-    for i in range(M):
-        idx = TimeWallCellId[4][i][0]  # 인접한 위쪽 단면도의 셀의 번호
-        idy = TimeWallCellId[2][0][i]  # 인접한 서쪽 단면도의 셀의 번호
-        Graph[idx][2] = idy
-        Graph[idy][3] = idx
-    # 북쪽 단면도와 인접한 셀들의 경우
-    for i in range(M):
-        idx = TimeWallCellId[4][0][i]  # 인접한 위쪽 단면도의 셀의 번호
-        idy = TimeWallCellId[3][0][M-1 - i]  # 인접한 북쪽 단면도의 셀의 번호
-        Graph[idx][3] = idy
-        Graph[idy][3] = idx
-
-    # 평면도에서 시간의 벽이 시작하는 셀의 행 번호, 열 번호
-    timewallStartx = -1
-    timewallStarty = -1
-
-    # 평면도에서 시간의 벽이 시작하는 셀의 위치를 탐색
-    for i in range(N):
-        for j in range(N):
-            if SpaceMap[i][j] == 3:
-                timewallStartx = i
-                timewallStarty = j
+    # [2] dr-1, dr+1 방향으로 동일처리, 대각선 원점잡고 dr방향 처리
+    for org_dr in ((dr-1)%8, (dr+1)%8):
+        si,sj = mi+di[org_dr], mj+dj[org_dr]        # 첫 대각선 위치부터 체크
+        while 0<=si<N and 0<=sj<N:                  # 대각선 방향으로 초기위치 탐색후 직선단위 처리
+            if v[si][sj] == 0 and marr[si][sj] > 0: # 전사 만나면 전사가 바라보는 방향 처리
+                v[si][sj]=1
+                cnt +=marr[si][sj]                  # 돌로만든 전사수 누적
+                mark_safe(v, si, sj, dr, org_dr)    # v에 dr방향으로 이동가능지역 표시
                 break
-        if timewallStartx != -1:
-            break
 
-    # 평면도와 인접하는 단면도 셀들에 대응되는 번호의 정점을 잇는 간선 추가
-    # 동쪽 단면도의 경우
-    if timewallStarty + M < N:
-        for i in range(M):
-            idx = TimeWallCellId[0][M-1][i]
-            idy = SpaceMapCellId[timewallStartx + (M - 1) - i][timewallStarty + M]
-            Graph[idx][1] = idy
-            Graph[idy][2] = idx
-    # 남쪽 단면도의 경우
-    if timewallStartx + M < N:
-        for i in range(M):
-            idx = TimeWallCellId[1][M-1][i]
-            idy = SpaceMapCellId[timewallStartx + M][timewallStarty + i]
-            Graph[idx][1] = idy
-            Graph[idy][3] = idx
-    # 서쪽 단면도의 경우
-    if timewallStarty > 0:
-        for i in range(M):
-            idx = TimeWallCellId[2][M-1][i]
-            idy = SpaceMapCellId[timewallStartx + i][timewallStarty - 1]
-            Graph[idx][1] = idy
-            Graph[idy][0] = idx
-    # 북쪽 단면도의 경우
-    if timewallStartx > 0:
-        for i in range(M):
-            idx = TimeWallCellId[3][M-1][i]
-            idy = SpaceMapCellId[timewallStartx - 1][timewallStarty + (M -1) - i]
-            Graph[idx][1] = idy
-            Graph[idy][1] = idx
+            ci,cj = si,sj                           # 첫 위치가 전사가 아닌 경우는 직선으로 내려오며 탐색
+            while 0<=ci<N and 0<=cj<N:              # 범위내라면 계속 진행
+                if v[ci][cj]==0:                    # 처음 방문
+                    v[ci][cj] = 1
+                    if marr[ci][cj] > 0:            # 전사로 막혔으면
+                        cnt +=marr[ci][cj]
+                        mark_safe(v,ci,cj,dr,org_dr)    # v에 dr방향으로 이동가능지역 표시
+                        break
+                else:
+                    break
+                ci,cj = ci+di[dr], cj+dj[dr]
 
-    return cnt
+            si,sj = si+di[org_dr], sj+dj[org_dr]
 
-N, M, E = map(int, input().split())
+    return v, cnt
 
-# 공간의 평면도 입력
-for i in range(N):
-    SpaceMap[i][:N] = list(map(int, input().split()))
+# move_cnt, attk_cnt = move_men(v, mi, mj)
+def move_men(v,mi,mj):
+    # (상하좌우), (좌우상하) 메두사 시야가 아니면 (!=1)
+    move, attk = 0, 0
 
-# 시간의 벽의 동쪽 단면도 입력
-for i in range(M):
-    TimeWall[0][i][:M] = list(map(int, input().split()))
-
-# 시간의 벽의 서쪽 단면도 입력
-# 구현의 편의를 위해서 TimeWall[2][][]에 서쪽 단면도 정보 저장
-for i in range(M):
-    TimeWall[2][i][:M] = list(map(int, input().split()))
-
-# 시간의 벽의 남쪽 단면도 입력
-# 구현의 편의를 위해서 TimeWall[1][][]에 남쪽 단면도 정보 저장
-for i in range(M):
-    TimeWall[1][i][:M] = list(map(int, input().split()))
-
-# 시간의 벽의 북쪽 단면도 입력
-for i in range(M):
-    TimeWall[3][i][:M] = list(map(int, input().split()))
-
-# 시간의 벽의 위쪽 단면도 입력
-for i in range(M):
-    TimeWall[4][i][:M] = list(map(int, input().split()))
-
-# 시간 이상 현상에 대한 정보 입력
-for i in range(1, E+1):
-    x, y, direction, cycle = map(int, input().split())
-    events[i].xpos = x
-    events[i].ypos = y
-    events[i].direction = direction
-    events[i].cycle = cycle
-    if events[i].direction == 1:
-        events[i].direction = 2
-    elif events[i].direction == 2:
-        events[i].direction = 1
-    events[i].alive = 1
-
-cnt = build_graph(N, M)
-dist = [-1] * (cnt + 1)
-
-# 장애물인 경우 도달할 수 없으므로 미리 아주 큰 값으로 세팅합니다
-# 평면도에 있는 장애물의 경우
-for i in range(N):
-    for j in range(N):
-        if SpaceMap[i][j] == 3:
-            continue
-        if SpaceMap[i][j] == 1:
-            idx = SpaceMapCellId[i][j]
-            dist[idx] = INF
-
-# 평면도에서 시간 이상 현상의 시작점도 도달 불가능하므로 장애물과 같이 처리합니다
-for i in range(1, E+1):
-    x = events[i].xpos
-    y = events[i].ypos
-    idx = SpaceMapCellId[x][y]
-    dist[idx] = INF
-
-# 단면도 위에 있는 장애물의 경우 역시 똑같이 처리합니다
-for t in range(5):
-    for i in range(M):
-        for j in range(M):
-            if TimeWall[t][i][j] ==1:
-                idx = TimeWallCellId[t][i][j]
-                dist[idx] = INF
-
-from collections import deque
-que = deque()
-
-cell_start = -1
-cell_end = -1
-
-# 탈출구 위치 탐색
-for i in range(N):
-    for j in range(N):
-        if SpaceMap[i][j] == 4:
-            cell_end = SpaceMapCellId[i][j]
-            break
-    if cell_end != -1:
-        break
-
-# 타임머신의 시작점 탐색
-for i in range(M):
-    for j in range(M):
-        if TimeWall[4][i][j] ==2:
-            cell_start = TimeWallCellId[4][i][j]
-            break
-    if cell_start != -1:
-        break
-
-que.append(cell_start)
-dist[cell_start] = 0
-
-runs = 1
-while True:
-    # 현재 턴에 확장하는 이상현상이 있으면 영향을 받는 셀을 업데이트합니다
-    for i in range(1, E+1):
-        if not events[i].alive:
-            continue
-        if runs % events[i].cycle:
-            continue
-        nx = events[i].xpos
-        ny = events[i].ypos
-        # 이상현상의 방향에 따라 영향을 받는 셀의 좌표를 구합니다
-        steps = runs // events[i].cycle
-        if events[i].direction == 0:
-            ny += steps
-        elif events[i].direction == 1:
-            nx += steps
-        elif events[i].direction == 2:
-            ny -= steps
-        else:
-            nx -= steps
-        if nx < 0 or ny < 0 or nx >= N or ny >= N:
-            events[i].alive = 0
-            continue
-        if SpaceMap[nx][ny]:
-            events[i].alive = 0
-            continue
-        idx = SpaceMapCellId[nx][ny]
-        dist[idx] = INF
-
-    # 이번턴에 도달 가능한 셀들의 번호를 저장할 리스트
-    next_cells = []
-    size = len(que)
-    for _ in range(size):
-        idx = que.popleft()
-        for i in range(4):
-            idy = Graph[idx][i]
-            if idy == -1:
+    for dirs in (((-1,0),(1,0),(0,-1),(0,1)), ((0,-1),(0,1),(-1,0),(1,0))):
+        for idx in range(len(men)-1,-1,-1):
+            ci,cj = men[idx]
+            if v[ci][cj]==1:                # 메두사 시야면 얼음!
                 continue
-            if dist[idy] != -1:
-                continue
-            dist[idy] = runs
-            next_cells.append(idy)
 
-    if not next_cells:
-        break
-    que.extend(next_cells)
-    if dist[cell_end] != -1:
-        break
-    runs += 1
+            dist = abs(mi-ci)+abs(mj-cj)    # 현재거리
+            for di,dj in dirs:
+                ni,nj = ci+di, cj+dj
+                # 범위내 메두사시야 아니고 현재보다 줄어드는 방향이면 (상하좌우 우선순위로 이동)
+                if 0<=ni<N and 0<=nj<N and v[ni][nj]!=1 and dist>abs(mi-ni)+abs(mj-nj):
+                    if (ni,nj)==(mi,mj):
+                        attk+=1             #
+                        men.pop(idx)
+                    else:
+                        men[idx]=[ni,nj]
+                    move+=1
+                    break
+    return move, attk
 
-# 정답을 출력합니다.
-# 불가능하면 -1이 출력됩니다
-if dist[cell_end] == -1 or dist[cell_end] >= INF:
+
+#######################################
+#######################################
+
+N, M = map(int, input().split())
+si,sj,ei,ej = map(int, input().split())
+tlst = list(map(int, input().split()))
+
+men = []
+for i in range(0,M*2,2):
+    men.append([tlst[i],tlst[i+1]])
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+# [0] BFS로 메두사 최단경로: 도로따라 공원까지(여러 개면 상하좌우 순) 없으면 -1
+route = find_route(si,sj,ei,ej)
+if route==-1:
     print(-1)
 else:
-    print(dist[cell_end])
+    for mi,mj in route:
+        move_cnt, attk_cnt = 0, 0
+
+        # [1] 메두사의 이동: 지정된 최단거리로 한 칸 이동 (전사 마주치면 삭제)
+        for i in range(len(men)-1, -1, -1):     # 삭제시 역순으로 접근
+            if men[i]==[mi,mj]:                 # 같은좌료
+                men.pop(i)
+
+        # [2] 메두사의 시선: 상하좌우 네 방향 가장 많이 돌로 만들 수 있는 방향찾기
+        # => v[]에 표시해서 이동시 참조(메두사시선 == 1, 전사에 가려진 곳 == 2, 빈 땅==0)
+        # marr[][]: 지도에 있는 전사수 표시
+        marr = [[0]*N for _ in range(N)]
+        for ti,tj in men:
+            marr[ti][tj]+=1
+
+        mx_stone = -1
+        v = []
+        for dr in (0, 4, 6, 2):     # 상하좌우 순서로 처리!
+            tv, tstone = make_stone(marr,mi,mj,dr)
+            if mx_stone<tstone:
+                mx_stone=tstone
+                v = tv
+        # print(mx_stone)
+        # myprint(v)
+        # myprint(marr)
+
+        # [3] 전사들의 이동(한 칸씩 두번): 메두사 있는 경우 공격
+        move_cnt, attk_cnt = move_men(v, mi, mj)
+
+        print(move_cnt, mx_stone, attk_cnt)
+    print(0)
