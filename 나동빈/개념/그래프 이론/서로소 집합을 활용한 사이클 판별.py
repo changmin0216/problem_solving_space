a15845b64ebf5757e44fe837c1e680148b1e8778
changmin <dufkawl0611@naver.com>
2025-04-21 21:01:55 +0900

    백준-2025/04/21

--- 나동빈/개념/그래프 이론/서로소 집합을 활용한 사이클 판별.py
+++ 나동빈/개념/그래프 이론/서로소 집합을 활용한 사이클 판별.py
@@ -12,19 +12,20 @@ def find_parent(parent, x):
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
     b = find_parent(parent, b)
-
     if a < b:
         parent[b] = a
     else:
         parent[a] = b
 
 v, e = map(int, input().split())
+
 parent = [0] * (v+1)
 
 for i in range(1, v+1):
     parent[i] = i
 
 cycle = False
+
 
 for i in range(e):
     a, b = map(int, input().split())
