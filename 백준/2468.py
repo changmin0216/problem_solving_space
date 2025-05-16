34a4c72cf0e7dfe444bbf0ab58205947de61f820
changmin <dufkawl0611@naver.com>
2025-05-16 16:10:50 +0900

    백준-2025/05/16

--- 백준/2468.py
+++ 백준/2468.py
@@ -1,5 +1,6 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
 dy = [1,-1,0,0]
 dx = [0,0,-1,1]
@@ -10,15 +11,17 @@ def dfs(y, x, h):
         ny, nx = y + dy[i], x + dx[i]
 
         if 0<=ny<n and 0<=nx<n:
-            if not visited[ny][nx] and graph[ny][nx] < h:
-                dfs(y, x, h)
+            if not visited[ny][nx] and graph[ny][nx] > h:
+                dfs(ny, nx, h)
+
+    return
 
 def solution(h):
     result = 0
 
     for i in range(n):
         for j in range(n):
-            if not visited[i][j] and graph < h:
+            if not visited[i][j] and graph[i][j] > h: ## 아직 방문 안했고
                 dfs(i, j, h)
                 result+=1
 
@@ -44,7 +47,9 @@ for i in graph:
     if max_ > max_num:
         max_num = max_
 
-
-for h in (min_num, max_num+1):
+answer = -1
+for h in range(min_num, max_num+1):
     visited = [[False] * n for _ in range(n)]
-    solution(h)
+    answer = max(solution(h), answer)
+
+print(answer)
