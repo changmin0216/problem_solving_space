366b356e8d271ee34cf659d893f388c6395dba71
changmin <dufkawl0611@naver.com>
2025-07-31 00:00:03 +0900

    swea-0728

--- 자바_코테/algorithm/src/baekjoon/BOJ1260.java
+++ 자바_코테/algorithm/src/baekjoon/BOJ1260.java
@@ -4,62 +4,73 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ1260 {
+
     static int n, m, v;
+    static ArrayList<Integer>[] graph;
+    static StringBuilder sb = new StringBuilder();
     static boolean[] visited;
-    static int[][] graph;
 
-    public static void dfs(int node) {
-        visited[node] = true;
+    static void bfs() {
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        q.offer(v);
+        visited[v] = true;
 
-        System.out.print(node+" ");
-        for (int i = 1; i < n + 1; i++) {
-            if (graph[node][i] == 1 && !visited[i]) {
-                dfs(i);
-            }
-        }
-    }
-
-    public static void bfs(int start) {
-        visited[start] = true;
-
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-
-        while (!queue.isEmpty()) {
-            int now = queue.poll();
-            System.out.print(now+" ");
-
-            for (int i = 1; i < n + 1; i++) {
-                if (graph[now][i] == 1 && !visited[i]) {
-                    queue.add(i);
-                    visited[i] = true;
+        while (!q.isEmpty()) {
+            int now = q.poll();
+            sb.append(now).append(" ");
+            for (Integer a : graph[now]) {
+                if (!visited[a]) {
+                    visited[a] = true;
+                    q.offer(a);
                 }
             }
         }
-
     }
-    public static void main(String[] args) throws IOException {
+
+    static void dfs(int node) {
+        visited[node] = true;
+
+        sb.append(node).append(" ");
+        for (Integer a : graph[node]) {
+            if (!visited[a]) {
+                dfs(a);
+            }
+        }
+    }
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = null;
+
 
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         v = Integer.parseInt(st.nextToken());
 
-        graph = new int[n+1][n+1];
+        graph = new ArrayList[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            graph[i] = new ArrayList<>();
+        }
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            graph[a][b] = graph[b][a] = 1;
+
+            graph[a].add(b);
+            graph[b].add(a);
         }
 
-        visited = new boolean[n+1];
+        for (int i = 0; i < n + 1; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        visited = new boolean[n + 1];
         dfs(v);
-        System.out.println();
-        visited = new boolean[n+1];
-        bfs(v);
+        sb.append("\n");
+        visited = new boolean[n + 1];
+        bfs();
+
+        System.out.println(sb);
     }
 }
