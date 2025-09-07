59fabfb98c8b0a7d86ef60fdb1cda149b96b6d33
changmin0216 <dufkawl0611@naver.com>
2025-09-07 22:36:36 +0900

    백준-09/07

--- 자바_코테/algorithm/src/baekjoon/Main_2042_구간합구하기.java
+++ 자바_코테/algorithm/src/baekjoon/Main_2042_구간합구하기.java
@@ -17,10 +17,11 @@ public class Main_2042_구간합구하기 {
 
         arr = new long[n];
         seg = new long[4*n];
+
         for (int i=0;i<n;i++) {
             arr[i] = Long.parseLong(br.readLine());
         }
-        init(1, 0, n-1);
+        init(1, 0, n - 1);
         for (int i=0;i<m+k;i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
@@ -34,33 +35,33 @@ public class Main_2042_구간합구하기 {
             }
         }
     }
-    static long init(int n, int l, int r){
+
+    static long init(int n, int l, int r) {
         if(l==r) return seg[n] = arr[l];
-        int mid = (l + r) / 2;
-        return seg[n] = init(n*2, l, mid) + init(n*2+1, mid+1, r);
+        int mid = (l+r) / 2;
+        seg[n] = init(n*2, l, mid) + init(n*2+1, mid+1, r);
+        return seg[n];
     }
 
     static long query(int n, int seg_s, int seg_e, int arr_s, int arr_e) {
-        if (seg_e < arr_s || seg_s > arr_e) {
-            return 0;
-        }
-        if (arr_s <= seg_s && seg_e <= arr_e) {
-            return seg[n];
-        }
+        if (arr_s > seg_e || arr_e < seg_s) return 0;
+        if (seg_s >= arr_s && seg_e <= arr_e) return seg[n];
         int mid = (seg_s + seg_e) / 2;
         return query(n*2, seg_s, mid, arr_s, arr_e) + query(n*2+1, mid+1, seg_e, arr_s, arr_e);
     }
 
-    static void update(int n, int l, int r, long diff, int index) {
-        if (index < l || index > r) return;
-        if(l==r) {
+    static void update(int n, int s, int e, long diff, int index) {
+        if (s > index || index > e) return;
+        if (s==e) {
             arr[index]+=diff;
             seg[n]+=diff;
             return;
         }
-        int mid = (l+r)/2;
-        update(n*2, l, mid, diff, index);
-        update(n*2+1, mid+1, r, diff, index);
+        int mid = (s+e)/2;
+        update(n*2, s, mid, diff, index);
+        update(n*2+1, mid+1, e, diff, index);
         seg[n] = seg[n*2] + seg[n*2+1];
     }
 }
+
+
